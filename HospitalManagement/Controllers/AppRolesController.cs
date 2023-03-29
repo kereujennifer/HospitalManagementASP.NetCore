ac548a94f87b4f60edbb37f370cd21c1ced1bb98
Jennifer Kereu <84716775+NIffer101@users.noreply.github.com>
2023-03-29 16:54:24 +0300

    commit

---
+++ HospitalManagement/Controllers/AppRolesController.cs
@@ -0,0 +1,50 @@
+﻿using HospitalManagement.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HospitalManagement.Controllers
+{
+    public class AppRolesController : Controller
+    {
+        private readonly RoleManager<IdentityRole> roleManager;
+
+        public AppRolesController(RoleManager<IdentityRole> roleManager) 
+        {
+            this.roleManager = roleManager;
+        }
+        public IActionResult Index()
+        {
+            var roles =  roleManager.Roles.ToList();
+            return View(roles);
+        }
+        [HttpGet]
+        public IActionResult CreateRole()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateRole(CreateRoleViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                IdentityRole identityRole = new IdentityRole
+                {
+                    Name = model.RoleName
+                };
+
+                IdentityResult result = await roleManager.CreateAsync(identityRole);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("index");
+                }
+                foreach (IdentityError error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+
+            }
+            return View(model);
+        }
+
+    }
+}
---
+++ HospitalManagement/ViewModels/EditRoleViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace HospitalManagement.ViewModels
+{
+    public class EditRoleViewModel
+    {
+    }
+}
