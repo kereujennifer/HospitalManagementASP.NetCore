02aaa90c73a2bf4da560f837bde8a00f1d0aa041
Jennifer Kereu <84716775+NIffer101@users.noreply.github.com>
2023-04-18 16:48:10 +0300

    sidebar curve plus js

--- HospitalManagement/wwwroot/js/site.js
+++ HospitalManagement/wwwroot/js/site.js
@@ -14,3 +14,25 @@ function confirmDelete(uniqueId, isDeleteClicked) {
         $('#' + confirmDeleteSpan).hide();
     }
 }
+const list = document.querySelectorAll('.list')
+function activeLink() {
+    list.forEach((item) =>
+        item.classList.remove('active'));
+    this.classList.add('active');
+    debugger
+    let id = this.getAttribute("id");
+    sessionStorage.setItem("sidebarMenu", id);
+
+}
+list.forEach((item) =>
+    item.addEventListener('click', activeLink));
+
+//change active class
+debugger
+const sideNav = sessionStorage.getItem("sidebarMenu");
+if (sideNav) {
+    list.forEach((item) =>
+        item.classList.remove('active'));
+    const elem = document.getElementById(sideNav);
+    elem.classList.add('active');
+}
