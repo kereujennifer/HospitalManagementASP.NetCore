1ba056940f129faa460cf802804b1964ffee0492
Jennifer Kereu <84716775+NIffer101@users.noreply.github.com>
2023-05-04 09:53:05 +0300

    add migration doctorandnurse

--- HospitalManagement/Migrations/20230504065138_doctorandnurse.Designer.cs
+++ HospitalManagement/Migrations/20230504065138_doctorandnurse.Designer.cs
@@ -4,6 +4,7 @@ using HospitalManagement.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace HospitalManagement.Migrations
 {
     [DbContext(typeof(ApplicationDBContext))]
-    partial class ApplicationDBContextModelSnapshot : ModelSnapshot
+    [Migration("20230504065138_doctorandnurse")]
+    partial class doctorandnurse
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
